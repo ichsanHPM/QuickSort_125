83bdff69eec47c03965ab9a33fb50d81f110deb2
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-03-30 15:07:36 -0800

    break

--- QuickSort_125.cpp
+++ QuickSort_125.cpp
@@ -6,6 +6,20 @@ int arr[20];                            //array integers to hold values
 int cmp_count = 0;                      //
 int mov_count = 0;                      //number of data movement
 int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+    }
+
+}
+
+
 int main()
 {
     std::cout << "Hello World!\n";
