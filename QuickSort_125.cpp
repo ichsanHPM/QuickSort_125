e2aecc4b2254a6839174f53e383c343c9bcec265
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-03-30 14:59:38 -0800

    Add project files.

---
+++ QuickSort_125.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];                            //array integers to hold values
+int cmp_count = 0;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
